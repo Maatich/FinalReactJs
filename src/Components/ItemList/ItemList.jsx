4497eef9c4f86d7f143afe4bbbc8c86cefaacba0
matias <matiichrem@gmail.com>
2023-01-26 20:22:38 -0300

    detalles esteticos

--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -1,10 +1,11 @@
 import Item from '../Item/Item'
 import React from 'react';
+import '../ItemList/itemList.css'
 
 const ItemList = ({ products }) => {
   return (
     <>
-      <div className='container d-flex titular'>
+      <div className='container  titular p-3'>
         <h1>Nuestros Servicios</h1>
       </div>
       <div className="row row-cols-1 row-cols-md-3 bg-black m-2 g-4">
